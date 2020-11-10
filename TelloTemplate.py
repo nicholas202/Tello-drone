b7fefa33e9273f79e0f39f6a6e691a4536b05d85
Nicholas Arce <02nicholas.arce@gulllakecs.org>
2020-11-10 10:45:10 -0800

    Converted square to a function

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -42,6 +42,13 @@ recvThread.start()
 
 
 # CREATE FUNCTIONS HERE....
+# Make a Square Function
+def square():
+    sendmsg('up 50')
+    for i in range(4):
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+
 
 
 print("\nNicholas Arce")
@@ -60,12 +67,8 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Make a Square
-        sendmsg('up 50')
-        for i in range(4):
-            sendmsg('forward 100')
-            sendmsg('ccw 90')
-            
+        # Call the Square Function
+        square()
 
         sendmsg('land')
 
